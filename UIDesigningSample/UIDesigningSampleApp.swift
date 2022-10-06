08220915474c0fcef1021aed4e12e5a9e1a57250
Keerthi-Sparkout <keerthi@sparkouttech.com>
2022-10-06 19:04:32 +0530

    Initial Commit

---
+++ UIDesigningSample/UIDesigningSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIDesigningSampleApp.swift
+//  UIDesigningSample
+//
+//  Created by Apple on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct UIDesigningSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ToolbarView()
+        }
+    }
+}
